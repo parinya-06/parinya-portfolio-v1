4b53052f5cf19bdfbd5998a495560f36c1177b70
parinya-06 <85719463+parinya-06@users.noreply.github.com>
2021-09-12 18:04:23 +0700

    update12092564

--- src/components/Projectss.js
+++ src/components/Projectss.js
@@ -16,6 +16,13 @@ import web4 from "../images/web4.png";
 import web4_1 from "../images/web4_1.png";
 import web4_2 from "../images/web4_2.png";
 import web4_3 from "../images/web4_3.png";
+import web5_1 from "../images/web5_1.png";
+import web5_2 from "../images/web5_2.png";
+import web5_3 from "../images/web5_3.png";
+import web5_4 from "../images/web5_4.png";
+import web5_5 from "../images/web5_5.png";
+import web5_6 from "../images/web5_6.png";
+import web5_7 from "../images/web5_7.png";
 
 const useStyles = makeStyles({
   root: {
@@ -30,60 +37,81 @@ export default function MediaCard() {
   const classes = useStyles();
 
   return (
-    <div style={{backgroundColor:'black',margin:'10px',padding:'10px'}}>
-    <center >
-      <Card className={classes.root}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            HTML and CSS Lab skills
-          </Typography>
-          <img src={web1} alt=""/>
-        </CardContent>
-      </CardActionArea>
-      <CardActionArea>
-        <CardContent>
-        
-          <Typography gutterBottom variant="h5" component="h2">
-            Copy Web Lab
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            เป็นงานฝึกเขียน Html,css,js โดยอาจาร์ยให้สร้างเว็บขึ้นมาให้เหมือนกับเว็บไซต์ที่สนใจ
-          </Typography>
-          <img src={web2} alt=""/>
-          <img src={web2_1} alt=""/>
-          <img src={web2_2} alt=""/>
-        </CardContent>
-      </CardActionArea>
-      <CardActionArea>
-        
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-          RESPONSIVE Lab
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            อาจาร์ยให้เขียนเว็บที่มีความ RESPONSIVE สามารถเแิดได้ในทุกอุปกรณ์
-          </Typography>
-          <img src={web3} alt=""/>
-        </CardContent>
-      </CardActionArea>
-      <CardActionArea>
-        
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-          Project Lab
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            เป็น Project รายวิชา การสร้างและออกแบบเว็บ 
-          </Typography>
-          <img src={web4} alt=""/>
-          <img src={web4_1} alt=""/>
-          <img src={web4_2} alt=""/>
-          <img src={web4_3} alt=""/>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </center>
+    <div className="projects" style={{ backgroundColor: 'black', margin: '10px', padding: '10px' }}>
+      <center >
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                HTML and CSS Lab skills
+              </Typography>
+              <img src={web1} alt="" />
+            </CardContent>
+          </CardActionArea>
+          <CardActionArea>
+            <CardContent>
+
+              <Typography gutterBottom variant="h5" component="h2">
+                Copy Web Lab
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                เป็นงานฝึกเขียน Html,css,js โดยอาจารย์ให้สร้างเว็บขึ้นมาให้เหมือนกับเว็บไซต์ที่สนใจ
+              </Typography>
+              <img src={web2} alt="" />
+              <img src={web2_1} alt="" />
+              <img src={web2_2} alt="" />
+            </CardContent>
+          </CardActionArea>
+          <CardActionArea>
+
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                RESPONSIVE Lab
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                อาจารย์ให้เขียนเว็บที่มีความ RESPONSIVE สามารถเปิดได้ในทุกอุปกรณ์
+              </Typography>
+              <img src={web3} alt="" />
+            </CardContent>
+          </CardActionArea>
+          <CardActionArea>
+
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Project Lab HTML+CSS+Javascipt
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                เป็น Project รายวิชา การสร้างและออกแบบเว็บ
+              </Typography>
+              <img src={web4} alt="" />
+              <img src={web4_1} alt="" />
+              <img src={web4_2} alt="" />
+              <img src={web4_3} alt="" />
+            </CardContent>
+          </CardActionArea>
+          <CardActionArea>
+
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Project Lab PHP
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                เป็น Project รายวิชา Webprogamming โดยอาจารย์ให้สร้างระบบสารสนเทศเพื่อจัดการข้อมูลนักศึกษาฝึกงาน และ สหกิจ โดยใช้ databasse เป็น sql มีฟังก์ชั่น CRUD ครบทุกอย่าง
+                และมีการใช้ภาษา PHP+AJAX ในการเขียนเว็บ
+              </Typography>
+              <img src={web5_1} alt="" />
+              <img src={web5_2} alt="" />
+              <img src={web5_7} alt="" />
+              <img src={web5_3} alt="" />
+              <img src={web5_6} alt="" />
+
+              <img src={web5_5} alt="" />
+              <img src={web5_4} alt="" />
+
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </center>
     </div>
   );
 }
